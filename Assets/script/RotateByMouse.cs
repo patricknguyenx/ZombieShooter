ed3c6499f66bbb1c1c6243655d0312435d7c77c1
patricknguyenx <moime2001@gmail.com>
2025-08-27 19:56:55 +0700

    fire

--- Assets/script/RotateByMouse.cs
+++ Assets/script/RotateByMouse.cs
@@ -4,14 +4,38 @@ using UnityEngine;
 
 public class RotateByMouse : MonoBehaviour
 {
-    public float anglePerSecond;
+    public float angleOverDistance;
+    public Transform cameraHolder;
+    public float minPitch;
+    public float maxPitch;
+
+    private float pitch;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
 
     private void Update()
     {
+        UpdateYaw();
+        UpdatePitch();
+    }
+
+    private void UpdateYaw()
+    {
         float mouseX = Input.GetAxis("Mouse X");
+        float deltaYaw = mouseX * angleOverDistance;
+        transform.Rotate(0, deltaYaw, 0);
+    }
+
+    private void UpdatePitch()
+    {
         float mouseY = Input.GetAxis("Mouse Y");
-        float yaw = mouseX * anglePerSecond * Time.deltaTime;
-        float yaw2 = mouseY * anglePerSecond * Time.deltaTime;
-        transform.Rotate(-yaw2, yaw, 0);
+        float deltaPitch = -mouseY * angleOverDistance;
+        pitch = Mathf.Clamp(pitch + deltaPitch, minPitch, maxPitch);
+        cameraHolder.localEulerAngles = new Vector3(pitch, 0, 0);
     }
 }
+
